fc80cddb17b31ec12a05ff2fd45d51fe381662fd
AndyLampert <lamperta@gmail.com>
2014-05-08 17:00:57 -0600

    set up locations, pages properly show current page in h1

--- app.js
+++ app.js
@@ -5,36 +5,72 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
-var locations = {
+var locations = [
+	'/iida',
+	'/minamiaizu',
+	'/aizuwakamatsu',
+	'/okazaki',
+	'/tokyo'
+];
 
-};
+var locationsFormatted = [
+	'Iida, Nagano',
+	'Minamiaizu, Fukushima',
+	'Aizuwakamatsu, Fukushima',
+	'Okazaki, Aichi',
+	'Tokyo'
+]
 
 // tells express to look in the public folder for ???
 app.use(express.static(__dirname + '/public'));
 
-// on hitting the / page, render 'index.jade'
-app.get('/', function(req, res) {
-	res.render('index');
-});
+var getNextLocation = function(currentLocation) {
+	// needs current page
+	for(var i=0; i<locations.length; i++){
+		// console.log(currentLocation, locations[i]);
+		if(currentLocation === locations[i]) {
+			nextPage = locations[i+1];
+			console.log(nextPage);
+			// stop execution of this function and give back a value
+			return nextPage;
+		}
+	}
+};
 
 app.get('/iida', function(req, res) {
-	res.render('iida');
+	res.render('iida', {
+		jadeVar: locationsFormatted[0]
+	});
+	var currentPage = req.path;
+	var nextUrl = getNextLocation(currentPage); 
+
+	// console.log(locations);
+	// console.log(currentPage);
+	// console.log(nextPage);
 });
 
 app.get('/minamiaizu', function(req, res){
 	res.render('minamiaizu');
+	var currentPage = req.path;
+	var nextUrl = getNextLocation(currentPage); 
 });
 
-app.get('/aizuwakamatsu', function(){
+app.get('/aizuwakamatsu', function(req, res){
 	res.render('aizuwakamatsu');
+	var currentPage = req.path;
+	var nextUrl = getNextLocation(currentPage); 
 });
 
-app.get('/okazaki', function(){
+app.get('/okazaki', function(req, res){
 	res.render('okazaki');
+	var currentPage = req.path;
+	var nextUrl = getNextLocation(currentPage); 
 });
 
-app.get('tokyo', function(){
+app.get('/tokyo', function(req, res){
 	res.render('tokyo');
+	var currentPage = req.path;
+	var nextUrl = getNextLocation(currentPage); 
 });
 
 // open the port 7047 and listen for page requests
